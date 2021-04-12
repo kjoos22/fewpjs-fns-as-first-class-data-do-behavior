bf7dcffceb03c5d342c6b68bbf83677ab0069289
kjoos22 <kjoos22@gmail.com>
2021-04-12 14:15:05 -0400

    commit tree 44c2b4f5c0bd0ddb4dfbf6468da4df9a9055af2c

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+const greetingArea = document.getElementById("greeting")
 
 /* Write your implementation of greet() */
+function greet(timeInput) {
+  if (timeInput.split(":")[0] < 12) {
+    return "Good Morning"
+  } else if (timeInput.split(":")[0] >= 12 && timeInput.split(":")[0] < 17) {
+    return "Good Afternoon"
+  } else if (timeInput.split(":")[0] >= 17) {
+    return "Good Evening"
+  }
+}
+
+
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  greetingArea.innerText = message
+}
